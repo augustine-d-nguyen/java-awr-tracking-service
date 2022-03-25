4abd667021de844d1cc1f5db98fbd1f523308283
augustine <augustine.nguyen@vincere.io>
2022-03-25 10:46:23 +0700

    change to ssh protocol

--- git-service/build.sh
+++ git-service/build.sh
@@ -18,11 +18,17 @@ echo "delete previous image."
 docker rmi "${IMG_NAME}" 2>/dev/null 1>&2 || true
 docker image prune -f
 
+# - Copy ssh key
+echo "Copy ssh pub key"
+cp ../ssh-key/awr_rsa.pub ./
+
 # - Build img
 echo "build docker image: ${IMG_NAME}"
 docker build -q -t ${IMG_NAME} -f Dockerfile . --no-cache
 
 # - Clean
+echo "Clean up"
 docker image prune --filter label=awr_git_img__stage=init
+rm -f ./awr_rsa.pub
 
 
